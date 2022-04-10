23d6dd1160039eeedb92df1f3d6db41ba53f7b8b
Joshua <joshuajmelnitzkysj@gmail.com>
2022-04-10 17:28:36 -0300

    Servidor con express

--- main.js
+++ main.js
@@ -1,26 +1,54 @@
-let fs = require('fs');
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+const server = app.listen(8080, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+ })
+ 
+
+app.get('/', (req, res) => {
+    res.send({ mensaje: 'hola mundo' });
+})
+
+
+app.get('/productos', (req, res) => {
+    producto.getAll().then(prod =>res.send(prod));
+})
+
+
+app.get('/productoRandom', (req, res) => {
+    producto.getAll().then( (resp) => {
+        let random = Math.round(Math.random() * (resp.length - 1) + 1);
+        producto.getById(random).then(resp => res.send(resp));
+    })
+})
+ 
+
 let id = 1;
+
 
 class Contenedor{
 
     constructor(name){
         this.name = name;
-        this.array = [];
     }
  
 
     save = async (obj) => {
         try{
-            let read = await fs.promises.readFile(`${this.name}.json`, "utf-8");
-            if (!read){
-                this.array.push(obj);
-                await fs.promises.writeFile(`${this.name}.json`, JSON.stringify(this.array.map(prod => ({...prod, id: 1})), null, 2), "utf-8");
-            }else{
+            
+            if(fs.existsSync(`${this.name}.json`)){
+                let read = await fs.promises.readFile(`${this.name}.json`, "utf-8");
                 let file = JSON.parse(read);
                 file.push(obj);
                 await fs.promises.writeFile(`${this.name}.json`, JSON.stringify(file.map(prod => ({...prod, id: id++})), null, 2), "utf-8");
                 return file.length;
+            }else{
+                await fs.promises.writeFile(`${this.name}.json`, JSON.stringify([{...obj, id: 1}], null, 2), "utf-8");
             }
+            
             return 1;
 
         }catch(err){
@@ -41,8 +69,7 @@ class Contenedor{
 
     getAll = async () => {
         try{
-            let read = JSON.parse(await fs.promises.readFile(`${this.name}.json`, "utf-8"));
-            return read;
+            return JSON.parse(await fs.promises.readFile(`${this.name}.json`, "utf-8"));
         }catch(err){
             console.log("Error en la lectura");
         }
@@ -52,9 +79,15 @@ class Contenedor{
     deleteById = async (num) => {
         try{
             let read = JSON.parse(await fs.promises.readFile(`${this.name}.json`, "utf-8"));
-            read.splice(num-1, 1);
 
-            await fs.promises.writeFile(`${this.name}.json`, JSON.stringify(read, null, 2), "utf-8");
+            let index = read.findIndex(prod => prod.id  === num);
+
+            if (index !== -1){
+                read.splice(prueba, 1);
+                await fs.promises.writeFile(`${this.name}.json`, JSON.stringify(read, null, 2), "utf-8");
+            }else{
+                console.log(`No existe producto con ID: ${num}`);
+            }
 
         }catch(err){
             console.log('Error al borrar');
@@ -63,7 +96,7 @@ class Contenedor{
 
 
     deleteAll = async () =>{
-        await fs.promises.writeFile(`${this.name}.json`, '', "utf-8");
+        await fs.promises.writeFile(`${this.name}.json`, '[]', "utf-8");
     }
 }
 
@@ -72,7 +105,7 @@ class Contenedor{
 let producto = new Contenedor("Productos");
 
 //  Save()  --> uncomment code below
-producto.save({title: "Escuadra", price: 123.45, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'}).then(res => console.log(res));
+// producto.save({title: "Escuadra", price: 123.45, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'}).then(res => console.log(res));
 
 // getById()  --> uncomment code below
 // producto.getById(1).then(res => console.log(res));
@@ -81,10 +114,8 @@ producto.save({title: "Escuadra", price: 123.45, thumbnail: 'https://cdn3.iconfi
 // producto.getAll().then(res => console.log(res));
 
 // deleteById()  --> uncomment code below
-//producto.deleteById(2);
+// producto.deleteById(2);
 
 // deleteAll()  --> uncomment code below
 // producto.deleteAll();
-
-
 
